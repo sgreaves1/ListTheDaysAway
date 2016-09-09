d6061bb3e0ac0b70c0464cce62bbdc4a38b6649c
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-09-09 14:50:29 +0100

    Add main window view model

---
+++ ListTheDaysAway/ListTheDaysAway/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ListTheDaysAway.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        /// <summary>
+        /// Initialises an instance of <see cref="MainWindowViewModel"/>.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
