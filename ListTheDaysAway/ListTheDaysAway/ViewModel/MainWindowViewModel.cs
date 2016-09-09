004ca8ad7da72ccf078843c738672731d66e1293
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-09-09 16:14:05 +0100

    Populate the events with fake data

--- ListTheDaysAway/ListTheDaysAway/ViewModel/MainWindowViewModel.cs
+++ ListTheDaysAway/ListTheDaysAway/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,40 @@
-﻿namespace ListTheDaysAway.ViewModel
+﻿using System;
+using System.Collections.ObjectModel;
+using ListTheDaysAway.Model;
+
+namespace ListTheDaysAway.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BaseViewModel
     {
+        /// <summary>
+        /// List of events to display to the user.
+        /// </summary>
+        private ObservableCollection<EventModel> _events = new ObservableCollection<EventModel>(); 
+
         /// <summary>
         /// Initialises an instance of <see cref="MainWindowViewModel"/>.
         /// </summary>
         public MainWindowViewModel()
         {
-            
+            PopulateEvents();
+        }
+
+        /// <summary>
+        /// List of events to display to the user.
+        /// </summary>
+        public ObservableCollection<EventModel> Events
+        {
+            get { return _events; }
+            set
+            {
+                _events = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void PopulateEvents()
+        {
+            Events.Add(new EventModel("Christmas", new DateTime(2016, 12, 25)));
         }
     }
 }
