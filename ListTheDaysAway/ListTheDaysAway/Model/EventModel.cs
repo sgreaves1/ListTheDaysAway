bdf02fcf31763d4f98d5c2931390e94c8739ca97
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-09-09 14:32:23 +0100

    Start adding event model

---
+++ ListTheDaysAway/ListTheDaysAway/Model/EventModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ListTheDaysAway.Model
+{
+    public class EventModel : BaseModel
+    {
+        private string _name;
+
+        public EventModel(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get {  return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
