17a6c902fbf6a8fac90c3f776740b09158e75f46
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-09-09 14:40:27 +0100

    Add end date to event model

--- ListTheDaysAway/ListTheDaysAway/Model/EventModel.cs
+++ ListTheDaysAway/ListTheDaysAway/Model/EventModel.cs
@@ -1,20 +1,52 @@
-﻿namespace ListTheDaysAway.Model
+﻿using System;
+
+namespace ListTheDaysAway.Model
 {
     public class EventModel : BaseModel
     {
+        /// <summary>
+        /// The name of the event.
+        /// </summary>
         private string _name;
 
-        public EventModel(string name)
+        /// <summary>
+        /// The date the event occurs.
+        /// </summary>
+        private DateTime _endDate;
+
+        /// <summary>
+        /// Initialises an instance of <see cref="EventModel"/>.
+        /// </summary>
+        /// <param name="name">The name of the event.</param>
+        /// <param name="endDate">The date the event occurs.</param>
+        public EventModel(string name, DateTime endDate)
         {
             Name = name;
+            EndDate = endDate;
         }
 
+        /// <summary>
+        /// The name of this event.
+        /// </summary>
         public string Name
         {
             get {  return _name; }
             set
             {
                 _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// The date this event occurs.
+        /// </summary>
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set
+            {
+                _endDate = value;
                 OnPropertyChanged();
             }
         }
